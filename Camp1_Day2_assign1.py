f3f68955c99edd42ac8459d3069d0b6fa790e3ed
vaibhavsolanki1193 <sancuriousvaibhav@gmail.com>
2018-05-29 09:20:10 +0530

    prentable

--- Camp1_Day2_assign1.py
+++ Camp1_Day2_assign1.py
@@ -1,6 +1,7 @@
 """
-Script to calculate number of session taken
-(dictionary, String manipulation)
+Cisco Black Belt: programability Level 1
+Script to calculate number of session taken (dictionary, String manipulation)
+submitted by: Vaibhav Solanki(vaibhav.solanki@agcnetworks.com)
 """
 Sessions_Attended = {'sessions' : '1011,2344,3222,44322,555,6332,721,8789,99,1011,1124,1245,137,1499'}
 session_list = Sessions_Attended['sessions'].split(',')
--- Camp1_Day2_assign2.py
+++ Camp1_Day2_assign2.py
@@ -1,5 +1,7 @@
 """
+Cisco Black Belt: programability Level 1
 Number guessing game
+submitted by: Vaibhav Solanki(vaibhav.solanki@agcnetworks.com)
 """
 
 import random
